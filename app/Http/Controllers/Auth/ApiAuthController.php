8babe863779ce17cc79eaac78604a00178639d10
shails25 <shailendra.phpdeveloper@gmail.com>
2021-03-19 19:38:08 +0530

    fix user model error

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\models\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;

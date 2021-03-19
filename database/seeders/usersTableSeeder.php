df695ab5ca42ea7f157a042a835bc9e819bd38ff
shails25 <shailendra.phpdeveloper@gmail.com>
2021-03-19 19:51:25 +0530

    seeder file fixed

--- database/seeders/usersTableSeeder.php
+++ database/seeders/usersTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
-class usersTableSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
